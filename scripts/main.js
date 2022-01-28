61e92d829f7e454932594fb6756a23f00ea7cd08
Atalata <kenny.lutz@laposte.net>
2022-01-28 19:04:27 +0100

    fixed sandwich menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,43 @@
+// Sandwich menu logic
+
+const bars = document.querySelector(".bars");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+bars.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    bars.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    bars.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+/***********************************************************/
+
+
+// Discord pseudo logic
+
 function initElement() {
     var discordLogo = document.getElementById("discord");
-    discordLogo.onclick = CopyToClipBar;
+    discordLogo.addEventListener("click", CopyToClipBar);
 }
 
 function CopyToClipBar() {
 
     navigator.clipboard.writeText("Atalata#2363")
         .then(() => {
-        console.log("Text copied to clipboard...");
+        console.log("Discord nickname copied to clipboard...");
         alert("Pseudo Discord copié !");
     })
         .catch(err => {
-        console.log('Something went wrong', err);
+        console.log("Something went wrong", err);
+        alert("I can't copy my discord nickname in your pc, here it is : Atalata#2363 !");
     })
 }
+
+/***********************************************************/
